4da95c5c6a5056a99542eed8b47b431dde3f85f8
Nithishka25 <nithishtk05@gmail.com>
2025-08-31 23:35:57 +0530

    hello world

---
+++ Exp1/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="AppContainer">
+      <h2>WELCOME</h2>
+      <div className="App">
+        <h1 className="moving-text">Hello World</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
